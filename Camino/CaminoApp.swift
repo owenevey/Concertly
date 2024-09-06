6a7d769ba6eff399db94d85cbbc33c8c18af0684
owenevey <owenevey@gmail.com>
2024-09-05 19:54:16 -0500

    Initial Commit

---
+++ Camino/CaminoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaminoApp.swift
+//  Camino
+//
+//  Created by Owen Evey on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CaminoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Camino/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Camino
+//
+//  Created by Owen Evey on 9/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
